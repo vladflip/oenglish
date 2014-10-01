97cc7d21e0b0944cae8ec5a0a076c47c06482034
vladflip <vladflip@vladflip>
2014-10-01 15:18:07 +0300

    sadf

--- js/script.js
+++ js/script.js
@@ -12,7 +12,27 @@ function getRandomColor() {
     return color;
 }
 
+HTMLElement.prototype.setClass = function(ins, out){
+	var cl = this.className;
+	var clarr = cl.split(' ');
 
+	console.log(clarr);
+	if(out&&clarr.length>1) clarr = clarr.slice(0, clarr.length-1);
+	else if(clarr.length===1) clarr = clarr.slice(0, 1);
+
+	this.className = clarr.join(' ') + ' ' + ins;
+}
+
+// PANE CLASS
+
+	function pane(el){
+
+		//DOM el
+		this.el = el;
+
+		this.coords = ['left','right','top','bottom'];
+
+	}
 
 // BLOCKS CLASS
 	function block(colors,words,element){
@@ -20,7 +40,11 @@ function getRandomColor() {
 
 		// DOM element
 		this.el = element;
-		 
+
+		//panes
+		this.pane = [new pane(this.el.getElementsByClassName('pane')[0]),
+						new pane(this.el.getElementsByClassName('pane')[1])];
+
 		// colors array
 		this.colors = colors;
 
@@ -28,17 +52,29 @@ function getRandomColor() {
 		this.words = words;
 
 		//content div
-		this.contentDiv = this.el.getElementsByClassName('content')[0];
+		this.contentDiv = [this.pane[0].el.getElementsByClassName('content')[0],
+							this.pane[1].el.getElementsByClassName('content')[0]];
 
 		this.animate = function(){
-			this.contentDiv.innerHTML = this.words[rand(0, this.words.length-1)];
+			this.contentDiv[0].innerHTML = this.words[rand(0, this.words.length-1)];
+			this.contentDiv[1].innerHTML = this.words[rand(0, this.words.length-1)];
 
 			// this.el.style.backgroundColor = this.colors[rand(0, this.colors.length-1)];
-			this.el.style.backgroundColor = getRandomColor();
+			this.pane[0].el.style.backgroundColor = getRandomColor();
+			this.pane[1].el.style.backgroundColor = getRandomColor();
 
 			setTimeout(function(){
-				self.animate();
-			},600);
+				self.pane[1].el.setClass('bottom',true);
+			},1000);
+
+			setTimeout(function(){
+				self.pane[0].el.setClass('center',true);
+				self.pane[1].el.setClass('center',true);
+			},rand(4,7)*1000);
+
+			// setTimeout(function(){
+			// 	self.animate();
+			// },rand(4,7)*1000);
 		}
 	};
 
